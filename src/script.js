f4e4853d061a0cb1ae4fe23498f029b3025d20bb
joyAct <joyleeact@gmail.com>
2020-11-16 22:14:26 +0900

    Feat: Closing a modal on click button

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
 
 openModalButtons.forEach((button) => {
   button.addEventListener('click', () => {
@@ -7,8 +8,21 @@ openModalButtons.forEach((button) => {
   });
 });
 
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
+
 function openModal(modal) {
   if (modal == null) return;
   modal.classList.add('active');
   overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
 }
